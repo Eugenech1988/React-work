85a2b123d6c17642e8961c8d7c8f87ab7e72fef6
eugene1988 <eugene@oneplanetops.com>
2017-09-20 11:54:32 +0300

    first commit

---
+++ src/components/header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = (props, context) => {
+  return (
+    <Header className="main-header">
+      <div className="wrapp">
+        <div className="container">
+          <div className="logo-wrapp">
+            <a href="" className="logo-link">
+              <img src="" alt="" className="logo-img"/>
+            </a>
+          </div>
+        </div>
+      </div>
+    </Header>
+  )
+
+};
+
+export default Header();
---
+++ src/containers/App/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+export function App() {
+  return (
+    <div className="wrapper">
+      <div className="content">
+
+      </div>
+    </div>
+  );
+}
+
+App.propTypes = {
+  children: PropTypes.node,
+};
+
+export default App;
