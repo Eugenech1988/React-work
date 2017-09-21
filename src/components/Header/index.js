d7159da426769a455c3cbd3677ea37c509250366
eugene1988 <eugene@oneplanetops.com>
2017-09-21 12:55:08 +0300

    css compiled

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import Nav from '../Nav';
 import NavItem from '../NavItem';
-import HeartLogo from '../../assets/icons/ReactHeart.svg';
 import './style.css';
 
 const navItems = [
