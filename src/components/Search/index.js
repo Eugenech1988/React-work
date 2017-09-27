b5b8d9c6937e26faa7c8974d130c13b2fa2f439d
eugene1988 <eugene@oneplanetops.com>
2017-09-27 18:20:23 +0300

    routes chages, name changes

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react';
 import './Style.css';
 
-class Application extends Component {
+class Search extends Component {
   render() {
     return (
-      <div className='app-wrapp'>
-        <form action='' className='search-form'>
-          <div className='input-wrapp'>
-            <input type='text' className='search-input' name='search-input' id='search-input'/>
-            <label htmlFor='search-input' className='input-label'></label>
-          </div>
-        </form>
+      <div className='app-wrap'>
+        <div className="container">
+          <form action='' className='search-form'>
+            <div className='input-wrap'>
+              <input type='text' className='search-input' name='search-input' id='search-input'/>
+              <label htmlFor='search-input' className='input-label'>
+                Search
+              </label>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-export default Application;
+export default Search;
