2412a8fddafe13ef73e16efea43edc34bae526a2
eugene1988 <eugene@oneplanetops.com>
2017-10-05 11:32:50 +0300

    sort function added

--- src/components/Search/ResultTable/index.js
+++ src/components/Search/ResultTable/index.js
@@ -4,6 +4,46 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 class ResultTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      IdReverse: false
+    });
+
+    this.headingIdHandlerClick = this.headingIdHandlerClick.bind(this);
+  }
+
+  contentSort(selector, sortDescending) {
+    const vals = [];
+
+    for (let i = 0, l = selector.length; i < l; i++) {
+      vals.push(selector[i].innerHTML);
+    }
+
+    vals.sort();
+
+    if (sortDescending) {
+      vals.reverse();
+    }
+
+    for (let i = 0, l = selector.length; i < l; i++) {
+      selector[i].innerHTML = vals[i];
+    }
+  }
+
+
+  headingIdHandlerClick() {
+    const Id = document.querySelectorAll('.table-content-id');
+
+    if (this.state.IdReverse) {
+      this.contentSort(Id, true);
+      this.setState({IdReverse: false});
+    } else {
+      this.contentSort(Id);
+      this.setState({IdReverse: true});
+    }
+  }
+
   render() {
     const resultValue = this.props.resultValue;
     return (
@@ -12,17 +52,20 @@ class ResultTable extends Component {
         <div className='wrap'>
           <div className='table-header'>
             <div className='table-col'>
-              <div className='heading'>
+              <div
+                className='heading heading-id'
+                onClick={this.headingIdHandlerClick}
+              >
                 Id
               </div>
             </div>
             <div className='table-col'>
-              <div className='heading'>
+              <div className='heading heading-name'>
                 Name
               </div>
             </div>
             <div className='table-col'>
-              <div className='heading'>
+              <div className='heading heading-full-name'>
                 Full Name
               </div>
             </div>
@@ -35,17 +78,17 @@ class ResultTable extends Component {
                   key={index}
                 >
                   <div className='table-col'>
-                    <div className='table-content'>
+                    <div className='table-content table-content-id'>
                       {item.id}
                     </div>
                   </div>
                   <div className='table-col'>
-                    <div className='table-content'>
+                    <div className='table-content table-content-name'>
                       {item.name}
                     </div>
                   </div>
                   <div className='table-col'>
-                    <div className='table-content'>
+                    <div className='table-content table-content-full-name'>
                       {item.full_name}
                     </div>
                   </div>
